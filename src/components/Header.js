1c743a27a16a0eed99afc48a1f1a775a9b535343
Jeff Tian <jeff.tian@outlook.com>
2020-12-09 17:55:22 +0800

    feat: add search box

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import _ from 'lodash';
+import React from 'react'
+import _ from 'lodash'
 
-import {toStyleObj, withPrefix} from '../utils';
-import Branding from './Branding';
-import Navigation from './Navigation';
+import {toStyleObj, withPrefix} from '../utils'
+import Branding from './Branding'
+import Navigation from './Navigation'
+import Search from './Search'
 
 export default class Header extends React.Component {
-    render() {
-        return (
-            <header id="masthead" className="site-header">
-              {_.get(this.props, 'image', null) ? (
-              <div id="header-bg" className="site-header-bg" style={toStyleObj('background-image:url(\'' + withPrefix(_.get(this.props, 'image', null)) + '\')')}/>
-              ) : (_.get(this.props, 'site.siteMetadata.header.background_img', null) && (
-              <div id="header-bg" className="site-header-bg" style={toStyleObj('background-image:url(\'' + withPrefix(_.get(this.props, 'site.siteMetadata.header.background_img', null)) + '\')')}/>
+  render() {
+    return (
+        <header id="masthead" className="site-header">
+          {_.get(this.props, 'image', null) ?
+              (
+                  <div id="header-bg" className="site-header-bg"
+                       style={toStyleObj('background-image:url(\'' +
+                           withPrefix(_.get(this.props, 'image', null)) +
+                           '\')')}/>
+              ) :
+              (_.get(this.props, 'site.siteMetadata.header.background_img',
+                  null) && (
+                  <div id="header-bg" className="site-header-bg"
+                       style={toStyleObj('background-image:url(\'' + withPrefix(
+                           _.get(this.props,
+                               'site.siteMetadata.header.background_img',
+                               null)) + '\')')}/>
               ))}
-              <div className="site-header-scroll">
-                <div className="site-header-inside">
-                  <div className="site-header-vertical">
-                    <Branding {...this.props} />
-                    <Navigation {...this.props} />
-                  </div>
+          <div className="site-header-scroll">
+            <div className="site-header-inside">
+              <div className="row">
+                <div className="site-header-top">
+                  <Search {...this.props} />
                 </div>
               </div>
-            </header>
-        );
-    }
+              <div className="row">
+                <div className="site-header-vertical">
+                  <Branding {...this.props} />
+                  <Navigation {...this.props} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+    )
+  }
 }
