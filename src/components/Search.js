795240e6fad5425b203b39e025de3ff2b5e7c37c
Jeff Tian <jeff.tian@outlook.com>
2020-12-09 18:14:20 +0800

    feat: placeholder for search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default class Search extends React.Component {
   render() {
-    return <input type="search" id="agolia-search"/>
+    return <input type="search" id="agolia-search" placeholder="Type to search... 输入文字进行搜索……"/>
   }
 }
