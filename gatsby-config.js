a28390278af2f1faa36dd32da4ccdee05036b999
Jeff Tian <jeff.tian@outlook.com>
2021-03-30 17:34:39 +0800

    feat: sync yuque docs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,42 @@
 const siteMetadata = require('./site-metadata.json')
 
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: siteMetadata,
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        `gatsby-transformer-remark`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`
-            }
-        },
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {}
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {}
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-            }
-        },
-        `gatsby-plugin-favicon`
-    ]
-};
+  pathPrefix: '/',
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-source-data`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {},
+    },
+    {
+      resolve: `@stackbit/gatsby-plugin-menus`,
+      options: {
+        sourceUrlPath: `fields.url`,
+        pageContextProperty: `menus`,
+      },
+    },
+    `gatsby-plugin-favicon`,
+    {
+      resolve: 'gatsby-source-yuque',
+      options: {
+        login: 'tian-jie',
+        repo: 'blog',
+        mdNameFormat: 'slug',
+      },
+    },
+  ],
+}
