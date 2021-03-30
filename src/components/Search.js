7631cf0c8f428aceeefaf0fc8dfc9b670b0fd18f
Jeff Tian <jeff.tian@outlook.com>
2021-03-30 14:00:40 +0800

    fix: lint issues

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default class Search extends React.Component {
   render() {
-    return <input type="search" id="agolia-search" placeholder="Type to search... 输入文字进行搜索……"/>
+    return <input type="search" id="agolia-search"
+                  placeholder="Type to search... 输入文字进行搜索……"
+                  aria-label="Search"/>
   }
 }
