3af0816661d231822feca3665fa2206514a21fa7
Jeff-Tian <Jeff-Tian@users.noreply.github.com>
2022-05-07 02:04:18 +0000

    deploy: 519602fa6470f626ffc63135f59dbfa5663c642e

---
+++ support-me.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Thanks for using Jeff Tian's packages! If you find they are useful to you, please consider buy him a cup of
+coffee: https://jeff-tian.jiwai.win/support-me"
+
+(curl -sf https://cachenet-jeff-tian.cloud.okteto.net/http/https%3A%2F%2Fapi.urlto.app%2Fv1%2Fscreenshoturl%2Fjpg%3Furl%3Dhttps%253A%252F%252Fjeff-tian.jiwai.win%252Fsupport-me%26fullpage%3Dtrue%26viewportwidth%3D1024%26viewportheight%3D1000%26ignorecache%3Dfalse%26region%3Dus | imgcat) || echo "https://jeff-tian.jiwai.win/support-me"
