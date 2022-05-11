ead0f5a0e4201b1495d91924f0faaf2de120b1c3
Jeff Tian <jeff.tian@outlook.com>
2022-05-11 10:08:53 +0800

    feat: replace raw pagination with antdesign pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Link} from "gatsby";
 import {rhythm} from "../utils/typography";
+import {UniheartPagination} from "@uniheart/experience.ui.uniheart-pagination/uniheart-pagination";
 
-export const Pagination = ({limit, skip, totalPages, currentPage}) => {
+const RawPagination = ({limit, skip, totalPages, currentPage}) => {
     const isFirst = currentPage === 1;
     const isLast = currentPage === totalPages;
     const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString();
@@ -51,4 +52,38 @@ export const Pagination = ({limit, skip, totalPages, currentPage}) => {
     </ul>;
 };
 
+const UniHeartPagination = ({skip, limit, currentPage, totalItems, totalPages}) => {
+    return <UniheartPagination limit={limit}
+                               currentPage={currentPage}
+                               skip={skip}
+                               showTotal={(t) => `共${t}篇`} total={totalItems}
+                               itemRender={(toPage, itemType, originalElement) => {
+                                   if (itemType === 'page') {
+                                       return <Link
+                                           to={`/${toPage > 1 ? toPage : ''}`}
+                                       >
+                                           {toPage}
+                                       </Link>
+                                   }
+
+                                   if (itemType === 'prev' && toPage > 0) {
+                                       return <Link to={`/${toPage > 1 ? toPage : ''}`} rel="prev">
+                                           ←
+                                       </Link>
+                                   }
+
+                                   if (itemType === 'next' && currentPage < totalPages) {
+                                       return <Link to={`/${toPage}`} rel="next">→</Link>
+                                   }
+
+                                   return originalElement;
+                               }}
+                               onChange={(page) => {
+                                   console.log('page = ', page, window)
+                                   window.location.pathname = `/${page > 1 ? page : ''}`;
+                               }}
+    />
+}
+
+const Pagination = UniHeartPagination;
 export default Pagination;
