4c73a0ced19cb1f6fc8a322f35abc5b10b7c3907
Jeff Tian <jeff.tian@outlook.com>
2022-05-13 15:43:02 +0800

    feat: add gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,72 +1,85 @@
 const siteMetadata = require('./site-metadata.json');
 require("dotenv").config({
-  path: `.env`,
+    path: `.env`,
 });
 
 module.exports = {
-  pathPrefix: '/',
-  siteMetadata: siteMetadata,
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-source-data`,
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        cssLoaderOptions: {
-          esModule: false,
-          modules: {
-            namedExport: false,
-          }
-        }
-      }
-    },
-    {
-      resolve: `gatsby-remark-page-creator`,
-      options: {},
-    },
-    {
-      resolve: `@stackbit/gatsby-plugin-menus`,
-      options: {
-        sourceUrlPath: `fields.url`,
-        pageContextProperty: `menus`,
-      },
-    },
-    {
-      resolve: '@jeff-tian/gatsby-source-yuque',
-      options: {
-        login: 'tian-jie',
-        repo: 'blog',
-        mdNameFormat: 'slug',
-      },
-    },
-    // {
-    //   resolve: 'gatsby-source-graphql',
-    //   options: {
-    //     typeName: 'GitHub',
-    //     fieldName: 'github',
-    //     url: 'https://api.github.com/graphql',
-    //     // HTTP headers
-    //     headers: {
-    //       // Learn about environment variables: https://gatsby.dev/env-vars
-    //       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-    //     },
-    //     // HTTP headers alternatively accepts a function (allows async)
-    //     // headers: async () => {
-    //     //   return {
-    //     //     Authorization: await getAuthorizationToken(),
-    //     //   }
-    //     // },
-    //     // Additional options to pass to node-fetch
-    //     fetchOptions: {},
-    //   },
-    // },
-  ],
+    pathPrefix: '/',
+    siteMetadata: siteMetadata,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-source-data`,
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                cssLoaderOptions: {
+                    esModule: false,
+                    modules: {
+                        namedExport: false,
+                    }
+                }
+            }
+        },
+        {
+            resolve: `gatsby-remark-page-creator`,
+            options: {},
+        },
+        {
+            resolve: `@stackbit/gatsby-plugin-menus`,
+            options: {
+                sourceUrlPath: `fields.url`,
+                pageContextProperty: `menus`,
+            },
+        },
+        {
+            resolve: '@jeff-tian/gatsby-source-yuque',
+            options: {
+                login: 'tian-jie',
+                repo: 'blog',
+                mdNameFormat: 'slug',
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Jeff Tian`,
+                short_name: `jeff-tian`,
+                start_url: `/`,
+                background_color: `#666666`,
+                theme_color: `#f45a4e`,
+                display: `standalone`,
+                icon: `src/favicon.png`,
+                crossOrigin: `use-credentials`,
+            },
+        },
+        // {
+        //   resolve: 'gatsby-source-graphql',
+        //   options: {
+        //     typeName: 'GitHub',
+        //     fieldName: 'github',
+        //     url: 'https://api.github.com/graphql',
+        //     // HTTP headers
+        //     headers: {
+        //       // Learn about environment variables: https://gatsby.dev/env-vars
+        //       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        //     },
+        //     // HTTP headers alternatively accepts a function (allows async)
+        //     // headers: async () => {
+        //     //   return {
+        //     //     Authorization: await getAuthorizationToken(),
+        //     //   }
+        //     // },
+        //     // Additional options to pass to node-fetch
+        //     fetchOptions: {},
+        //   },
+        // },
+    ],
 };
