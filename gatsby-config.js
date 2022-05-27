0d20204eb1e65a629b5059c1d8cf4620380bd4a8
Jeff Tian@Workstation <jie.tian@live.cn>
2022-05-27 12:06:17 +0800

    feat: rss feed:

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,38 +5,24 @@ require("dotenv").config({
 
 module.exports = {
     pathPrefix: '/',
-    siteMetadata: siteMetadata,
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        `gatsby-transformer-remark`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`,
-            },
+    siteMetadata: { ...siteMetadata, siteUrl: 'https://jeff-tian.jiwai.win' },
+    plugins: [`gatsby-plugin-react-helmet`, `gatsby-source-data`, `gatsby-transformer-remark`, {
+        resolve: `gatsby-source-filesystem`, options: {
+            name: `pages`, path: `${__dirname}/src/pages`,
         },
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {
+    }, {
+            resolve: `gatsby-plugin-sass`, options: {
                 cssLoaderOptions: {
-                    esModule: false,
-                    modules: {
+                    esModule: false, modules: {
                         namedExport: false,
                     }
                 }
             }
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {},
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
+        }, {
+            resolve: `gatsby-remark-page-creator`, options: {},
+        }, {
+            resolve: `@stackbit/gatsby-plugin-menus`, options: {
+                sourceUrlPath: `fields.url`, pageContextProperty: `menus`,
             },
         },
         {
@@ -81,5 +67,52 @@ module.exports = {
         //     fetchOptions: {},
         //   },
         // },
-    ],
+
+
+        {
+            resolve: `gatsby-plugin-feed`, options: {
+                query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `, feeds: [{
+                    serialize: ({ query: { site, allMarkdownRemark } }) => {
+                        return allMarkdownRemark.edges.map(edge => {
+                            return Object.assign({}, edge.node.frontmatter, {
+                                description: edge.node.excerpt,
+                                date: edge.node.frontmatter.date,
+                                url: site.siteMetadata.siteUrl + edge.node.fields.name,
+                                guid: site.siteMetadata.siteUrl + edge.node.fields.absolutePath,
+                                custom_elements: [{ "content:encoded": edge.node.html }],
+                            })
+                        })
+                    }, query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { absolutePath name }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `, output: "/rss.xml", title: "Jeff Tian 的博客",
+                },],
+            },
+        },],
 };
