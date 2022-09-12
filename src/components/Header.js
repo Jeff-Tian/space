e9287244fd684b0a60994c048234dec56d31a112
Jeff Tian@Workstation <jie.tian@live.cn>
2022-09-12 17:48:03 +0800

    feat: add zhihu module

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,42 +5,30 @@ import {toStyleObj, withPrefix} from '../utils'
 import Branding from './Branding'
 import Navigation from './Navigation'
 import Search from './Search'
+import ZhihuWall from "./ZhihuWall";
 
 export default class Header extends React.Component {
-  render() {
-    return (
-        <header id="masthead" className="site-header">
-          {_.get(this.props, 'image', null) ?
-              (
-                  <div id="header-bg" className="site-header-bg"
-                       style={toStyleObj('background-image:url(\'' +
-                           withPrefix(_.get(this.props, 'image', null)) +
-                           '\')')}/>
-              ) :
-              (_.get(this.props, 'site.siteMetadata.header.background_img',
-                  null) && (
-                  <div id="header-bg" className="site-header-bg"
-                       style={toStyleObj('background-image:url(\'' + withPrefix(
-                           _.get(this.props,
-                               'site.siteMetadata.header.background_img',
-                               null)) + '\')')}/>
-              ))}
-          <div className="site-header-scroll">
-            <div className="site-header-inside">
-              <div className="row">
-                <div className="site-header-top">
-                  <Search {...this.props} />
+    render() {
+        return (<header id="masthead" className="site-header">
+            {_.get(this.props, 'image', null) ? (<div id="header-bg" className="site-header-bg"
+                                                      style={toStyleObj('background-image:url(\'' + withPrefix(_.get(this.props, 'image', null)) + '\')')}/>) : (_.get(this.props, 'site.siteMetadata.header.background_img', null) && (
+                <div id="header-bg" className="site-header-bg"
+                     style={toStyleObj('background-image:url(\'' + withPrefix(_.get(this.props, 'site.siteMetadata.header.background_img', null)) + '\')')}/>))}
+            <div className="site-header-scroll">
+                <div className="site-header-inside">
+                    <div className="row">
+                        <div className="site-header-top">
+                            <Search {...this.props} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="site-header-vertical">
+                            <Branding {...this.props} />
+                            <Navigation {...this.props} />
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="site-header-vertical">
-                  <Branding {...this.props} />
-                  <Navigation {...this.props} />
-                </div>
-              </div>
             </div>
-          </div>
-        </header>
-    )
-  }
+        </header>)
+    }
 }
---
+++ src/components/ZhihuWall.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Link} from "../utils";
+
+const ZhihuWall = () => <Link to="https://www.zhihu.com/consult/people/1073548674713423872" target="_blank"><img
+    src="https://urlscan.io/liveshot/?width=400&height=650&url=https://www.zhihu.com/consult/people/1073548674713423872"
+    alt="欢迎来知乎向我咨询任何问题！"/></Link>
+
+export default ZhihuWall
