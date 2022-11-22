d689ccdf0ce3a9078c451feb3f1c16e62a8382e3
cnjeftia <jeff.tian@outlook.com>
2022-11-22 19:18:05 +0800

    feat: failOn none to allow image processing failure

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,22 @@ module.exports = {
     siteMetadata: {...siteMetadata, siteUrl: 'https://jeff-tian.jiwai.win'},
     plugins: [
         `gatsby-plugin-image`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-plugin-sharp`,
+            options: {
+                // Defaults used for gatsbyImageData and StaticImage
+                defaults: {},
+                // Set to none to allow builds to continue on image errors
+                failOn: `none`,
+                // deprecated options and their defaults:
+                base64Width: 20,
+                forceBase64Format: `png`, // valid formats: png,jpg,webp
+                useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+                stripMetadata: true,
+                defaultQuality: 50,
+            },
+        },
+        // `gatsby-transformer-sharp`,
 
         `gatsby-plugin-react-helmet`, `gatsby-source-data`,
         {
