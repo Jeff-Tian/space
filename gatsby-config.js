ff891526930fdf15f61aff56e3911440347e15ad
cnjeftia <jeff.tian@outlook.com>
2022-12-03 11:58:30 +0800

    feat: set failOnError to false to gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,11 +11,9 @@ module.exports = {
         {
             resolve: `gatsby-plugin-sharp`,
             options: {
-                // Defaults used for gatsbyImageData and StaticImage
                 defaults: {},
-                // Set to none to allow builds to continue on image errors
                 failOn: `none`,
-                // deprecated options and their defaults:
+                failOnError: false,
                 base64Width: 20,
                 forceBase64Format: `png`, // valid formats: png,jpg,webp
                 useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
