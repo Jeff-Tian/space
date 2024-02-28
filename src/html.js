42927b0cc49d9927859a4ecd1318f7fda1e9cf2d
Jeff Tian <jeff.tian@outlook.com>
2024-02-28 13:32:25 +0800

    feat: add google ads

--- src/html.js
+++ src/html.js
@@ -6,17 +6,20 @@ export default function HTML(props) {
         <html {...props.htmlAttributes}>
         <head>
             {props.headComponents}
-            <link rel="stylesheet" href="/algolia/algoliasearchNetlify.css" type="text/css" />
+            <link rel="stylesheet" href="/algolia/algoliasearchNetlify.css" type="text/css"/>
+            <script async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9332882982479681"
+                    crossOrigin="anonymous"></script>
         </head>
         <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <noscript key="noscript" id="gatsby-noscript">
-          This app works best with JavaScript enabled.
+            This app works best with JavaScript enabled.
         </noscript>
         <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{__html: props.body}}
         />
         {props.postBodyComponents}
         <script src="/algolia/algoliasearchNetlify.js" type="text/javascript"></script>
@@ -26,10 +29,10 @@ export default function HTML(props) {
 }
 
 HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
+    htmlAttributes: PropTypes.object,
+    headComponents: PropTypes.array,
+    bodyAttributes: PropTypes.object,
+    preBodyComponents: PropTypes.array,
+    body: PropTypes.string,
+    postBodyComponents: PropTypes.array,
 }
